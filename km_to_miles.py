caae63bcd886f0588207e4eccd1a05eb7f4a1627
sumeetby16 <sumeetby16>
2025-10-30 15:05:03 +0530

    convertion

---
+++ km_to_miles.py
@@ -0,0 +1,4 @@
+km=float(input("enter the number to convert kilometeres to miles"))
+miles=km*0.621
+print(f"{km}={miles}")
+write the python code to decide whethere which  is smaller and bigger according to user input
