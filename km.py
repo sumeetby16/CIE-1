712614c16c6b9c5dd2d3ea07fb229f88f42a4214
sumeetby16 <sumeetby16>
2025-10-30 15:17:56 +0530

    convertion M To KM

--- km.py
+++ km.py
@@ -1,3 +1,8 @@
-km=float(input("enter the number to convert kilometer to miles"))
+km=float(input("enter the number to convert kilometer to miles:"))
 miles=km*0.621
 print(f"{km}={miles}")
+
+miles=float(input("enter the number to convert miles to kilometer:"))
+km=0.621/km
+print(f"{miles}={km}")
+
